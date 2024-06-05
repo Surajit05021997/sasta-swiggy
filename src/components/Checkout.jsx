af4eae3951cc1bf6b7e510cb584e1605a6a70669
Surajit Maity <surajitmaity12345@gmail.com>
2024-06-06 02:35:43 +0530

    feat: added checkout

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -1,10 +1,43 @@
-import { useContext } from "react";
+import { useContext, useState, useEffect } from "react";
 import RestaurantContext from "../utilities/RestaurantContext";
+import CartContext from "../utilities/CartContext";
 import { IMAGE_URL } from '../constants/constants.js';
 import './Checkout.css';
+import emptyCartImg from '../assets/empty_cart.webp';
+import { useNavigate } from 'react-router-dom';
+import AddButton from './AddButton.jsx';
+
 
 const Checkout = () => {
-  const { checkoutRestaurant } = useContext(RestaurantContext);
+  const [totalFoodAmount, setTotalFoodAmount] = useState(0);
+  const [totalAmount, setTotalAmount] = useState(0);
+  const { checkoutRestaurant, setSelectedRestaurant } = useContext(RestaurantContext);
+  const { cart } = useContext(CartContext);
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    setSelectedRestaurant(checkoutRestaurant);
+  }, []);
+
+  useEffect(() => {
+    const total = cart.reduce((totalFoodAmount, item) => totalFoodAmount = totalFoodAmount + ((item.price/100)*item.quantity), 0);
+    setTotalFoodAmount(total);
+    setTotalAmount(total + ((5*totalFoodAmount)/100) + 5);
+  }, [cart, totalFoodAmount]);
+
+  if(!checkoutRestaurant) {
+    return (
+      <div className="checkout">
+        <div className="empty-checkout">
+          <img className="empty-cart-img" src={emptyCartImg} />
+          <p className="title">Your cart is empty</p>
+          <p className="sub-title">You can go to home page to view more restaurants</p>
+          <button className="home-btn" onClick={() => navigate('/')}>SEE RESTAURANTS</button>
+        </div>
+      </div>
+    );
+  }
   const { name, cloudinaryImageId, areaName } = checkoutRestaurant;
   return (
     <div className="checkout">
@@ -15,8 +48,40 @@ const Checkout = () => {
           <div>{areaName}</div>
         </div>
       </div>
-      <div className="food-items"></div>
-      <div className="bill-details"></div>
+      <div className="cart-item-list">
+        {
+          cart.map((foodItem) => {
+            return (
+              <div className="cart-item" key={foodItem.id}>
+                <div className="left-info">{foodItem.name}</div>
+                <div className="right-info">
+                  <AddButton id={foodItem.id} name={foodItem.name} price={foodItem.price} />
+                  <div className="item-price">₹{(foodItem.price * foodItem.quantity)/100}</div>
+                </div>
+              </div>
+            )
+          })
+        }
+      </div>
+      <div className="bill-details">
+        <div>Bill Details</div>
+        <div className="bill-detail-row">
+          <div>Item Total</div>
+          <div>₹{totalFoodAmount}</div>
+        </div>
+        <div className="bill-detail-row">
+          <div>Platform fee</div>
+          <div>₹5</div>
+        </div>
+        <div className="bill-detail-row">
+          <div>GST and Restaurant Charges</div>
+          <div>₹{(5*totalFoodAmount)/100}</div>
+        </div>
+      </div>
+      <div className="total-amount">
+        <div>TO PAY</div>
+        <div>₹{totalAmount}</div>
+      </div>
     </div>
   );
 }
