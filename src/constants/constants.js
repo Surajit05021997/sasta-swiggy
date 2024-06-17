a40a5acba07bc1f808998dcdbdad14c22820906c
Surajit Maity <surajitmaity12345@gmail.com>
2024-06-18 01:44:25 +0530

    added netlify config

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,5 +1,10 @@
-export const SWIGGY_API_URL = 'https://thingproxy.freeboard.io/fetch/https://www.swiggy.com/dapi/restaurants/list/v5?is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING';
+// export const SWIGGY_API_URL = 'https://thingproxy.freeboard.io/fetch/https://www.swiggy.com/dapi/restaurants/list/v5?is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING';
+// export const IMAGE_URL = 'https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/';
+// export const RESTAURANT_MENU = 'https://thingproxy.freeboard.io/fetch/https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true';
+// export const PRE_SEARCH_URL = 'https://thingproxy.freeboard.io/fetch/https://www.swiggy.com/dapi/landing/PRE_SEARCH';
+// export const SEARCH_SUGGEST_URL = 'https://thingproxy.freeboard.io/fetch/https://www.swiggy.com/dapi/restaurants/search/v3?submitAction=SUGGESTION&&selectedPLTab=DISH';
+export const SWIGGY_API_URL = 'dapi/restaurants/list/v5?is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING';
 export const IMAGE_URL = 'https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/';
-export const RESTAURANT_MENU = 'https://thingproxy.freeboard.io/fetch/https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true';
-export const PRE_SEARCH_URL = 'https://thingproxy.freeboard.io/fetch/https://www.swiggy.com/dapi/landing/PRE_SEARCH';
-export const SEARCH_SUGGEST_URL = 'https://thingproxy.freeboard.io/fetch/https://www.swiggy.com/dapi/restaurants/search/v3?submitAction=SUGGESTION&&selectedPLTab=DISH';
+export const RESTAURANT_MENU = 'https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true';
+export const PRE_SEARCH_URL = 'https://www.swiggy.com/dapi/landing/PRE_SEARCH';
+export const SEARCH_SUGGEST_URL = 'https://www.swiggy.com/dapi/restaurants/search/v3?submitAction=SUGGESTION&&selectedPLTab=DISH';
