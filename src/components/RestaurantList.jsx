2788fc2cb32fde2a18d0a0e5508f21f1332feeb7
Surajit Maity <surajitmaity12345@gmail.com>
2024-06-25 16:18:00 +0530

    showing message for unserviceable location

--- src/components/RestaurantList.jsx
+++ src/components/RestaurantList.jsx
@@ -5,20 +5,28 @@ import { Link } from 'react-router-dom';
 import './RestaurantList.css';
 
 const RestaurantList = () => {
-  const {restaurants, restaurantListTitle} = useFetchRestaurants();
+  const {restaurants, restaurantListTitle, restaurantNotAvailableData} = useFetchRestaurants();
+  console.log(restaurantNotAvailableData)
+  console.log(restaurants)
   return (
     <div>
       <div className="restaurant-list-title">{restaurantListTitle}</div>
       <section className="restaurant-list">
         {
-          restaurants?.length === 0 ? <RestaurantListShimmer /> :
-          restaurants?.map((restaurant) => {
+          !restaurants && !restaurantNotAvailableData ? <RestaurantListShimmer /> :
+          ( !restaurants && restaurantNotAvailableData ? (
+            <div className="restaurant-not-available">
+              <img src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_476,h_476/portal/m/location_unserviceable.png" alt="No restaurant available image" />
+              <div className="title">Location Unserviceable</div>
+              <div>We don’t have any services at you location till now.</div>
+            </div>
+          ) : (restaurants?.map((restaurant) => {
             return (
               <Link to={`/restaurant/${restaurant.info.id}`} key={restaurant.info.id}>
                 <RestaurantTile restaurant={restaurant} />
               </Link>
             );
-          })
+          })))
         }
       </section>
     </div>
--- src/utilities/useFetchRestaurants.jsx
+++ src/utilities/useFetchRestaurants.jsx
@@ -5,8 +5,9 @@ import useLocation from './useLocation';
 import baseUrl from './getBaseUrl';
 
 const useFetchRestaurants = () => {
-  const [restaurants, setRestaurants] = useState([]);
+  const [restaurants, setRestaurants] = useState(null);
   const [restaurantListTitle, setRestaurantListTitle] = useState('');
+  const [restaurantNotAvailableData, setRestaurantNotAvailableData] = useState(null);
 
   const location = useLocation();
 
@@ -19,13 +20,15 @@ const useFetchRestaurants = () => {
   
   const fetchRestaurantData = async () => {
     const restaurantData = await axios.get(`${baseUrl}${SWIGGY_API_URL}&lat=${location.lat}&lng=${location.lng}`);
+    const restaurantNotAvailableDataResponse = restaurantData?.data?.data?.cards.find((card) => card?.card?.card?.id === 'swiggy_not_present');
+    setRestaurantNotAvailableData(restaurantNotAvailableDataResponse?.card?.card);
     const restaurantListData = restaurantData?.data?.data?.cards.find((res) => res?.card?.card?.gridElements?.infoWithStyle['@type'] === 'type.googleapis.com/swiggy.presentation.food.v2.FavouriteRestaurantInfoWithStyle');
     const restaurantsList = restaurantListData?.card?.card?.gridElements?.infoWithStyle?.restaurants;
     setRestaurants(restaurantsList);
-    const restaurantListTitleData = restaurantData?.data?.data?.cards.find((card) => card?.card?.card?.id === 'popular_restaurants_title').card.card.title;
+    const restaurantListTitleData = restaurantData?.data?.data?.cards.find((card) => card?.card?.card?.id === 'popular_restaurants_title')?.card?.card?.title;
     setRestaurantListTitle(restaurantListTitleData);
   }
-  return {restaurants, restaurantListTitle};
+  return {restaurants, restaurantListTitle, restaurantNotAvailableData};
 }
 
 export default useFetchRestaurants;
