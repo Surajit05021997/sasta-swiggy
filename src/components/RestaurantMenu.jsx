cf6490cde38bb79baab67890aa18206a83c21c15
Surajit Maity <surajitmaity12345@gmail.com>
2024-07-29 22:39:12 +0530

    added service error page for restaurant menu page

--- src/components/RestaurantMenu.jsx
+++ src/components/RestaurantMenu.jsx
@@ -1,6 +1,7 @@
 import { useParams } from "react-router-dom";
 import useFetchRestaurantMenu from '../utilities/useFetchRestaurantMenu.jsx';
 import RestaurantInfoTile from "./RestaurantInfoTile.jsx";
+import ServiceErrorPage from "./ServiceErrorPage.jsx";
 import RestaurantMenuTile from "./RestaurantMenuTile.jsx";
 import './RestaurantMenu.css';
 import { useState, useContext, useEffect } from 'react';
@@ -8,6 +9,7 @@ import RestaurantContext from '../utilities/RestaurantContext';
 import CartContext from "../utilities/CartContext";
 import { useNavigate } from 'react-router-dom';
 import RestaurantMenuShimmer from "./shimmer/RestaurantMenuShimmer.jsx";
+import { useSelector } from 'react-redux';
 
 const RestaurantMenu = () => {
   const [totalItem, setTotalItem] = useState(0);
@@ -16,6 +18,7 @@ const RestaurantMenu = () => {
   const { setSelectedRestaurant } = useContext(RestaurantContext);
   const { cart } = useContext(CartContext);
   const navigate = useNavigate();
+  const error = useSelector((state) => state.error);
 
   useEffect(() => {
     const total = cart?.reduce((acc, item) => acc = acc + item.quantity, 0);
@@ -26,43 +29,45 @@ const RestaurantMenu = () => {
     setSelectedRestaurant(restaurantMenu?.restaurantInfo);
   }, [restaurantMenu, setSelectedRestaurant]);
 
-  if(restaurantMenu) {
-    const { restaurantInfo, restaurantMenuInfo } = restaurantMenu;
     return (
-      <div>
-        <div className="restaurant">
-          <RestaurantInfoTile restaurantInfo={restaurantInfo}/>
-          <section className="restaurant-menu">
-            <h2>Menu</h2>
-            {
-              restaurantMenuInfo.map((restaurantMenuTileInfo, index) => {
-                return <RestaurantMenuTile
-                          key={restaurantMenuTileInfo.card.card.title}
-                          restaurantMenuTileInfo={restaurantMenuTileInfo}
-                          isFirstTile={index === 0 ? true : false}
-                        />
-              })
-            }
-          </section>
-        </div>
-        <div>
-          {
-            cart?.length > 0 ? (
-              <div className="cart-notification">
-                <div className="cart-notification-container">
-                  <div>{totalItem} items added</div>
-                  <div onClick={() => navigate('/checkout')}>VIEW CART</div>
-                </div>
-              </div>
-            ) : ''
-          }
-        </div>
-      </div>
+      error.serviceError ? (
+        <ServiceErrorPage />
+      ) : (
+        restaurantMenu ? (
+          <div>
+            <div className="restaurant">
+              <RestaurantInfoTile restaurantInfo={restaurantMenu.restaurantInfo}/>
+              <section className="restaurant-menu">
+                <h2>Menu</h2>
+                {
+                  restaurantMenu.restaurantMenuInfo.map((restaurantMenuTileInfo, index) => {
+                    return <RestaurantMenuTile
+                              key={restaurantMenuTileInfo.card.card.title}
+                              restaurantMenuTileInfo={restaurantMenuTileInfo}
+                              isFirstTile={index === 0 ? true : false}
+                            />
+                  })
+                }
+              </section>
+            </div>
+            <div>
+              {
+                cart?.length > 0 ? (
+                  <div className="cart-notification">
+                    <div className="cart-notification-container">
+                      <div>{totalItem} items added</div>
+                      <div onClick={() => navigate('/checkout')}>VIEW CART</div>
+                    </div>
+                  </div>
+                ) : ''
+              }
+            </div>
+          </div>
+        ) : (
+          <RestaurantMenuShimmer />
+        )
+      )
     )
-  } else {
-    return (<RestaurantMenuShimmer />);
-  }
-  
 }
 
 export default RestaurantMenu;
--- src/utilities/useFetchRestaurantMenu.jsx
+++ src/utilities/useFetchRestaurantMenu.jsx
@@ -3,11 +3,14 @@ import { RESTAURANT_MENU } from '../constants/constants.js';
 import { useState, useEffect } from 'react';
 import useLocation from './useLocation';
 import baseUrl from './getBaseUrl';
+import { useDispatch } from 'react-redux';
+import { updateServiceError } from '../store/errorSlice';
 
 const useFetchRestaurantMenu = (restaurantId) => {
   const [restaurantMenu, setRestaurantMenu] = useState(null);
 
   const location = useLocation();
+  const dispatch = useDispatch();
 
   useEffect(() => {
     if(restaurantId && location.lat && location.lng) {
@@ -16,17 +19,23 @@ const useFetchRestaurantMenu = (restaurantId) => {
   }, [restaurantId, location]);
 
   const fetchRestaurantMenuData = async (restaurantId) => {
-    const restaurantMenuData = await axios.get(`${baseUrl}${RESTAURANT_MENU}&restaurantId=${restaurantId}&lat=${location.lat}&lng=${location.lng}`);
-    const restaurantInfo = restaurantMenuData.data.data.cards.find((res) =>
-      res.card.card['@type'] === 'type.googleapis.com/swiggy.presentation.food.v2.Restaurant').card.card.info;
-    const restaurantMenuInfoWrapper = restaurantMenuData.data.data.cards.find((res) =>
-      res?.groupedCard?.cardGroupMap?.REGULAR?.cards.find((card) =>
-          card?.card?.card['@type'] === 'type.googleapis.com/swiggy.presentation.food.v2.ItemCategory'));
-    const restaurantMenuInfo = restaurantMenuInfoWrapper?.groupedCard?.cardGroupMap?.REGULAR?.cards.filter((res) => res.card.card['@type'] === 'type.googleapis.com/swiggy.presentation.food.v2.ItemCategory');
-    setRestaurantMenu({
-      restaurantInfo,
-      restaurantMenuInfo,
-    });
+    try {
+      const restaurantMenuData = await axios.get(`${baseUrl}${RESTAURANT_MENU}&restaurantId=${restaurantId}&lat=${location.lat}&lng=${location.lng}`);
+      const restaurantInfo = restaurantMenuData.data.data.cards.find((res) =>
+        res.card.card['@type'] === 'type.googleapis.com/swiggy.presentation.food.v2.Restaurant').card.card.info;
+      const restaurantMenuInfoWrapper = restaurantMenuData.data.data.cards.find((res) =>
+        res?.groupedCard?.cardGroupMap?.REGULAR?.cards.find((card) =>
+            card?.card?.card['@type'] === 'type.googleapis.com/swiggy.presentation.food.v2.ItemCategory'));
+      const restaurantMenuInfo = restaurantMenuInfoWrapper?.groupedCard?.cardGroupMap?.REGULAR?.cards.filter((res) => res.card.card['@type'] === 'type.googleapis.com/swiggy.presentation.food.v2.ItemCategory');
+      setRestaurantMenu({
+        restaurantInfo,
+        restaurantMenuInfo,
+      });
+    } catch(error) {
+      console.log('working')
+      dispatch(updateServiceError(true));
+    }
+    
   }
 
   return restaurantMenu;
