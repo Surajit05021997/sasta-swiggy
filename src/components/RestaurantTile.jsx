d0477528c792131828d5b5ef75d589d7bdb84fa3
Surajit Maity <surajitmaity12345@gmail.com>
2024-08-22 01:35:02 +0530

    showing discount info on food tile

--- src/components/RestaurantTile.jsx
+++ src/components/RestaurantTile.jsx
@@ -4,7 +4,7 @@ import './RestaurantTile.css';
 
 const RestaurantTile = (props) => {
 
-  const { name, areaName, cloudinaryImageId, cuisines, sla, avgRating } = props.restaurant.info;
+  const { name, areaName, cloudinaryImageId, cuisines, sla, avgRating, aggregatedDiscountInfoV3,aggregatedDiscountInfoV2 } = props.restaurant.info;
 
   const getRatingClassName = () => {
     const ratingColorClass = avgRating >= 4 ? 'rating-green' : 'rating-red';
@@ -15,8 +15,15 @@ const RestaurantTile = (props) => {
     <div className={props.isTopRestaurant ? "restaurant-tile top-restaurant" : "restaurant-tile"}>
       <div className="restaurant-tile-image-container">
         <img className="restaurant-tile-image" src={`${IMAGE_URL}${cloudinaryImageId}`} alt="Restaurant Image" />
+        {
+          aggregatedDiscountInfoV3 ? (
+            <div className="discount-info">{`${aggregatedDiscountInfoV3?.header ? aggregatedDiscountInfoV3.header : ''} ${aggregatedDiscountInfoV3?.subHeader ? aggregatedDiscountInfoV3.subHeader : ''}`}</div>
+          ) : (
+            <div className="discount-info">{`${aggregatedDiscountInfoV2?.descriptionList ? aggregatedDiscountInfoV2.descriptionList[0].meta.substring(0, aggregatedDiscountInfoV2.descriptionList[0].meta.indexOf('|')) : ''}`}</div>
+          )
+        }
       </div>
-      <div className="restaurant-tile-info">
+      <div className="restaurant-tile-info">  
         <div className="restaurant-name">{name}</div>
         <div className="restaurant-tile-info-group">
           <div className={getRatingClassName()}>{avgRating}</div>
