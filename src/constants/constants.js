ccbbdc89670ea8c1b048b84aa4940b8d881e34b4
Surajit Maity <surajitmaity12345@gmail.com>
2026-02-27 22:53:07 +0530

    updated constants

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,6 +1,7 @@
 export const SWIGGY_API_URL = '/dapi/restaurants/list/v5?is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING';
 export const SWIGGY_MOBILE_API_URL = '/mapi/homepage/getCards';
 export const IMAGE_URL = 'https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/';
-export const RESTAURANT_MENU = '/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true';
+// export const RESTAURANT_MENU = '/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true';
+export const RESTAURANT_MENU = '/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&catalog_qa=undefined&submitAction=ENTER';
 export const PRE_SEARCH_URL = '/dapi/landing/PRE_SEARCH';
 export const SEARCH_SUGGEST_URL = '/dapi/restaurants/search/v3?submitAction=SUGGESTION&&selectedPLTab=DISH';
